f34988072e95e94707dd082660145fabeebaa451
wayanputri <wayanputriyanti1502@gmail.com>
2023-09-28 11:49:57 +0700

    set up config and middleware

--- app/middlewares/jwt-middlewares.go
+++ app/middlewares/jwt-middlewares.go
@@ -1,1 +1,26 @@
 package middlewares
+
+import (
+	"project-capston/app/config"
+
+	"github.com/golang-jwt/jwt/v5"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+)
+
+func JWTMiddleware() echo.MiddlewareFunc {
+	return echojwt.WithConfig(echojwt.Config{
+		SigningKey:    []byte(config.JWT_SECRRET),
+		SigningMethod: "HS256",
+	})
+}
+
+func ExtractTokenUserId(e echo.Context) int {
+	user := e.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		userId := claims["userId"].(float64)
+		return int(userId)
+	}
+	return 0
+}
--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"project-capston/app/config"
+	"project-capston/app/database"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	cfg := config.InitConfig()
+	database.InitMysql(cfg)
+	// database.InittialMigration(mysql)
+
+	e := echo.New()
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
+	}))
+	// router.InitRouter(mysql, e)
+	e.Logger.Fatal(e.Start(":80"))
+
+}
